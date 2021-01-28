06b822404a2ee35936113d432ae069b5f487e391
Vany.Liu <vany8462@gmail.com>
2021-01-28 13:49:49 +0800

    fix excel download url path

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("list", views.list, name="list"),
     path("draw", views.draw, name="draw"),
     path(r'^export/xls/$', views.export_users_xls_verylucky,name='export_users_xls_verylucky'),
-    path(r'^export/xls/$', views.export_users_xls,name='export_users_xls'),
+    path(r'^export/xls/$2', views.export_users_xls,name='export_users_xls'),
 ]
 
